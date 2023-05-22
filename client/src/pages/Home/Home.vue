47ea197908d4757973980cf37a9fecc529ede8c9
aytmamatov_musa <musaaytmamatov@gmail.com>
2023-05-22 16:42:16 +0600

    Split the home page and added new rules for eslint

---
+++ client/src/pages/Home/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <el-row :gutter="10">
+      <Place v-for="col in 6" :key="col"/>
+    </el-row>
+  </div>
+</template>
+
+<script setup>
+import Place from '@/pages/Home/Place.vue';
+</script>
