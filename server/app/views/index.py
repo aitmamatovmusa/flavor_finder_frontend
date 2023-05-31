f87e042bd7915dd070dd34497c63aeb823cb6275
aytmamatov_musa <musaaytmamatov@gmail.com>
2023-05-31 14:49:46 +0600

    Supported getting data from the frontend

--- server/app/views/index.py
+++ server/app/views/index.py
@@ -1,5 +1,5 @@
 from flask_cors import cross_origin
-from flask import jsonify
+from flask import jsonify, request
 
 def index_views(app):
     from app.models import Place
@@ -21,3 +21,19 @@ def index_views(app):
                 }
             )
         return jsonify(places)
+
+    @app.route("/add-new-place", methods=["POST"])
+    def add_new_place():
+        if request.method == "POST":
+            new_place = request.get_json()
+
+            if not new_place:
+                return jsonify(error="Invalid json"), 400
+
+            name = new_place["name"]
+            address = new_place["address"]
+
+            if not name or not address:
+                return jsonify(error="Missing required fields"), 400
+
+        return []
