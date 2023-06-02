61ea23a6f632252867e1e34feae02854468a982a
aytmamatov_musa <musaaytmamatov@gmail.com>
2023-06-02 13:13:42 +0600

    Fetched the place from the backend

--- client/src/pages/DetailPlace.vue
+++ client/src/pages/DetailPlace.vue
@@ -5,6 +5,24 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import instance from '@/services/api';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const place = ref();
+async function fetchPlace(id) {
+  const { data } = await instance.get(`/api/place/${id}`);
+  place.value = data;
+}
+
+onMounted(() => {
+  const placeId = route.params?.id;
+  if (placeId) {
+    fetchPlace(placeId);
+  }
+});
 
 </script>
 
